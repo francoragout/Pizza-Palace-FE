e0453093ca120a36ab4e02849bd7405f7b3b888f
francoragout <francoragout1@gmail.com>
2023-04-18 09:16:18 -0300

    pages were created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Menu from "./pages/home/Menu";
+import Request from "./pages/home/Request";
+import User from "./pages/home/User";
+import Menus from "./pages/admin/menus";
+import Requests from "./pages/admin/Requests";
+import Users from "./pages/admin/Users";
+
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/request" element={<Request />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/menus" element={<Menus />} />
+        <Route path="/requests" element={<Requests />} />
+        <Route path="/users" element={<Users />} />
+      </Routes>
+    </Router>
+    
   );
 }
 
