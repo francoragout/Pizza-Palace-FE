7dffb8e9d1d4c42c279b659eea8d8178ce02dbe1
francoragout <francoragout1@gmail.com>
2023-04-21 20:58:13 -0300

    modal component was midified

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -5,16 +5,10 @@ const Modal = () => {
   return (
     <div className="modal fade" id="exampleModal" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
       <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header">
-            
-          </div>
+        <div className="modal-content">         
           <div className="modal-body">
             <Accordion />
-          </div>
-          <div className="modal-footer">
-            
-          </div>
+          </div>       
         </div>
       </div>
     </div>
