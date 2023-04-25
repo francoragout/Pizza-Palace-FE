5654c1c583c3ed27d51a23af5dfd50227a40150f
francoragout <francoragout1@gmail.com>
2023-04-25 03:37:57 -0300

    signup component was finished

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,16 +2,14 @@ import React from 'react'
 
 const Button = (props) => {
   return (
-    <li className='nav-item'>
-      <button 
-      type="button"
-      className={props.className}
-      data-bs-toggle={props.toggle}
-      data-bs-target={props.target}
-      aria-controls={props.controls}>
-        <i className={props.icon}></i> {props.name} 
-      </button>
-    </li>
+    <button 
+    type="button"
+    className={props.className}
+    data-bs-toggle={props.toggle}
+    data-bs-target={props.target}
+    aria-controls={props.controls}>
+      <i className={props.icon}></i> {props.name} 
+    </button>
   )
 }
 
--- src/components/Forms/Signup.js
+++ src/components/Forms/Signup.js
@@ -1,69 +1,119 @@
 import React from 'react'
-import { useForm } from 'react-hook-form'
+import { useState } from 'react'
 
 const Signup = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const [name, setName] = useState('');
+  const [nameValid, setNameValid] = useState(false);
+  function handleNameChange(event) {
+    setName(event.target.value);
+    setNameValid(event.target.value.match(/^[a-zA-Z]{3,20}$/));
+  }
+
+  const [lastname, setLastname] = useState('');
+  const [lastnameValid, setLastnameValid] = useState(false);
+  function handleLastnameChange(event) {
+    setLastname(event.target.value);
+    setLastnameValid(event.target.value.match(/^[a-zA-Z]{3,20}$/));
+  }
+  
+  const [email, setEmail] = useState('');
+  const [emailValid, setEmailValid] = useState(false);
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+    setEmailValid(event.target.value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i));
+  }
+  
+  const [password, setPassword] = useState('');
+  const [lengthValid, setLengthValid] = useState(false);
+  const [containsNumber, setContainsNumber] = useState(false);
+  const [containsUpperCase, setContainsUpperCase] = useState(false);
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+    setLengthValid(event.target.value.length >= 8);
+    setContainsNumber(/\d/.test(event.target.value));
+    setContainsUpperCase(/[A-Z]/.test(event.target.value));
+  }
+  
+  const [formValid, setFormValid] = useState(false);
+  const isFormValid = name && lastname && email && lengthValid && containsNumber && containsUpperCase;
+  if (formValid !== isFormValid) {
+    setFormValid(isFormValid);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
 
   return (
-    <form onSubmit={handleSubmit()}>
-
-      <div class="form-floating mb-3">
-        <input type="text" className="form-control" placeholder="Nombre" minLength="2" maxLength="20" 
-        {...register('name', { required: true })} />
-        <label>Nombre</label>
+    <form onSubmit={handleSubmit}>
+      <div className="form-floating mb-1">
+        <input 
+        type="text" 
+        maxLength="20"
+        className="form-control" 
+        id="name" 
+        placeholder="Nombre" 
+        onChange={handleNameChange}
+        value={name}/>
+        <label htmlFor="name">Nombre</label>
+      </div>
+      <div className='mb-3'>
+      {!nameValid && <span className='text-secondary'>Este campo es requerido.</span>}
       </div>
 
-      <div class="form-floating mb-3">
-        <input type="text" className="form-control" placeholder="Apellido" minLength="2" maxLength="20" 
-        {...register('lastname', { required: true })} />
-        <label>Apellido</label>
+      <div className="form-floating mb-1">
+        <input 
+        type="text" 
+        maxLength="20"
+        className="form-control" 
+        id="lastname" 
+        placeholder="Apellido" 
+        onChange={handleLastnameChange}
+        value={lastname}/>
+        <label htmlFor="lastname">Apellido</label>
+      </div>
+      <div className='mb-3'>
+      {!lastnameValid && <span className='text-secondary'>Este campo es requerido.</span>}
       </div>
 
-      <div class="input-group mb-3">
-        <span class="input-group-text"><i class="bi bi-envelope"></i></span>
-        <div class="form-floating">
-          <input type="email" class="form-control" placeholder="Correo electrónico" minLength="4" maxLength="30" 
-          {...register('email', { required: true })} />
-          <label>Correo electrónico</label>
+      <div className="input-group mb-1">
+        <span className="input-group-text"><i className="bi bi-envelope"></i></span>
+        <div className="form-floating">
+          <input 
+          type="email" 
+          maxLength="30"
+          className="form-control" 
+          id="email" 
+          placeholder="Correo electrónico" 
+          onChange={handleEmailChange} 
+          value={email}/>
+          <label htmlFor="email">Correo electrónico</label>
         </div>
-        {errors.eamil && (
-          <span className='text-secondary'>
-            El mail ingresado ya existe
-          </span>
-        )}
-      </div>  
-
-      <div class="form-floating mb-3">
-        <input type="text" className="form-control" placeholder="Dirección" minLength="4" maxLength="30" 
-        {...register('address', { required: true })} />
-        <label>Dirección</label>
+      </div>
+      <div className='mb-3'>
+      {!emailValid && <span className='text-secondary'>Ingrese un email válido.</span>}
       </div>
 
-      <div class="form-floating mb-3">
-        <input type="number" className="form-control" placeholder="Número de telefono" min="10000000"
-        max="999999999999"  
-        {...register('phone', { required: true })} />
-        <label>Número de telefono</label>
+      <div className="input-group mb-1">
+        <span className="input-group-text"><i className="bi bi-key"></i></span>
+        <div className="form-floating">
+          <input 
+          type="password" 
+          maxLength="30" 
+          className="form-control" 
+          id="password" 
+          placeholder="Contraseña" 
+          onChange={handlePasswordChange}
+          value={password}/>
+          <label htmlFor="password">Contraseña</label>
+        </div>         
       </div>
-
-      <div class="input-group mb-3">   
-        <span class="input-group-text"><i class="bi bi-question-circle"></i></span>
-        <div class="form-floating">
-          <input type="password" class="form-control" placeholder="Contraseña" pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^\w\d\s:])([^\s]){8,16}$"
-          {...register('password', { required: true })} />
-          <label>Contraseña</label>
-        </div>
-      </div>  
-
-      <button type="submit" class="btn btn-secondary">Registrarse</button>
-
-
-
-
-      
-      
-      
-
+      <div className='d-flex flex-column mb-3'>      
+        {!lengthValid && <span className='text-secondary'>Debe tener al menos 8 caracteres.</span>}
+        {!containsNumber && <span className='text-secondary'>Debe contener al menos un número.</span>}
+        {!containsUpperCase && <span className='text-secondary'>Debe contener al menos una letra mayúscula.</span>}
+      </div>
+      <button type="submit" className="btn btn-secondary" disabled={!formValid}>Registrarse</button>
     </form>
   )
 }
